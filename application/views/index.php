d8262f39d78d385650aed094752c65b4e3e54083
Luis G Romero <luisgromero@live.com>
2013-06-12 01:24:52 -0700

    Created Site controller
    Routed to "Site" controller as root and created a simple view
    "index.php"

---
+++ application/views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>My Favorite Places</title>
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
