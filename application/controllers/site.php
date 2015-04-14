cabfcc0d9114640f21527d4800a50503213e064c
Luis G Romero <luisgromero@live.com>
2015-04-14 00:24:08 -0700

    Update site.php controller - remove comments.

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -1,33 +1,23 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Site extends CI_Controller {
-
 	
-	public function index()
-	{
+	public function index(){
             $this->load->model('places');
             $data['googleMapsQuery']="http://maps.google.com/?q=";
             $data['placeAddress']=$this->places->getAllPlaces();
             $this->load->view("index",$data);
-                   
 	}
         
         public function addPlace(){
             $this->load->model('places');
             $place=$_POST['place'];
             $this->places->insertPlace($place);
-            //echo "You are adding a Place!" . $place;
-            //echo( $data[0]->id + "<br>");
-            //echo($data[0]->place);
         }
         
         public function removePlace(){
             $this->load->model('places');
             $place=$_POST['place'];
             $this->places->deletePlace($place);
-           
         }
 }
-
-/* End of file site.php */
-/* Location: ./application/controllers/site.php */
