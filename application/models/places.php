bf41b903bd1d7d7120f2772b5494e64c52a08aff
Luis G Romero <luisgromero@live.com>
2015-04-14 00:22:29 -0700

    Update comments on places.php model.

--- application/models/places.php
+++ application/models/places.php
@@ -1,24 +1,19 @@
 <?php
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of places
- */
 class Places extends CI_Model {
-
+    
+    // Gets all places from DB
     public function getAllPlaces(){
         $query = $this->db->query('SELECT * FROM places');
         return $query->result();     
     }
     
+    // Saves place to DB
     public function insertPlace($place){
         $data=  array("place"=>$place);
         $this->db->insert('places',$data);
     }
     
+    // Deletes place from DB
     public function deletePlace($place){
         $this->db->where('place',$place);
         $this->db->delete('places');        
